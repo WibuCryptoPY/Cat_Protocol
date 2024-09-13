0ce15199d385b13354ab027744cf290209d33c62
wibucrypto <56129456+wibucrypto2201@users.noreply.github.com>
2024-09-13 23:10:23 +0700

    Update cat.sh

--- cat.sh
+++ cat.sh
@@ -94,7 +94,7 @@ install_env_and_full_node() {
     log "Creating mint script..."
     echo '#!/bin/bash
 
-    command="sudo yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5 --fee-rate $(curl -s https://explorer.unisat.io/fractal-mainnet/api/bitcoin-info/fee | jq '.data.fastestFee')"
+    command="sudo yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5"
 
     while true; do
         $command
@@ -140,7 +140,8 @@ check_wallet_balance() {
 
 # Display main menu
 echo -e "\n
-${COLOR_GREEN}Welcome to WibuCrypto.${COLOR_RESET}
+${COLOR_GREEN}Welcome_WIBU_CRYPTO${COLOR_RESET}
+This script is completely free and open source.
 Please choose an operation as needed:
 1. Install dependencies and full node
 2. Create wallet
